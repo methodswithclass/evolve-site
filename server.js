139f0550c16656fa4af308f19976cf73d66d5c8e
Christopher Polito <cpolito343@gmail.com>
2017-12-16 12:54:12 -0500

    update

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ else {
 
 
 
-var listener = app.listen(port, function () {
+var listener = app.listen(process.env.PORT || 8080, function () {
 
 	console.log("listening on port", listener.address().port);
 });
