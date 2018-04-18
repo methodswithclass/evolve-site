7231bc28feaea3e8a10bd4bbb2248b0a41e7f911
Christopher Polito <cpolito343@gmail.com>
2018-04-18 00:32:17 -0400

    update

--- git.sh
+++ git.sh
@@ -1,12 +1,99 @@
-cd #!/bin/bash
+#!/bin/bash
 
 
 cd /home/cpolito/@stuff/@programs/evolve1
+
+constName1="commitAndPush"
+constName2="pushOnly"
 
 message="$1"
 remote="$2"
 branch="$3"
 
+operation=$constName1
 
-git add -A && git commit -m $message
-git push $remote $branch
+
+if [[ -z $branch ]]; 
+then
+	
+	operation=$constName2
+	message=""
+	remote="$1"
+	branch="$2"
+fi
+
+
+commit () {
+
+	git add -A && git commit -m $message
+}
+
+
+push() {
+
+	git push $remote $branch
+}
+
+
+checkPush() {
+
+
+	if [[ -n $remote && -n $branch ]]; 
+	then
+
+		push
+	fi
+
+}
+
+
+commitAndPush() {
+
+
+	if [[ -n $branch && -n $message && $operation == $constName1 ]]; 
+	then
+
+		commit
+
+		checkPush		
+	fi
+
+}
+
+
+pushOnly() {
+
+
+	if [[ $operation == $constName2 ]];
+	then
+
+		checkPush
+	fi
+
+}
+
+
+
+
+run() {
+
+
+
+	if [[ -n $branch && -n $message ]];
+	then
+
+		commitAndPush
+	else
+
+		pushOnly
+	fi
+
+}
+
+
+
+run
+
+
+
+
