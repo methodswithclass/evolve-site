a65e3351ca36c298bffc2685a47797d069e18814
Christopher Polito <cpolito343@gmail.com>
2018-08-01 15:46:19 -0400

    update

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -28,7 +28,7 @@ const errorHandler  = function () {
 	return function (err, req, res, next) {
 	  	
 		if (res.statusCode >= 500) {
-			res.send({error:err});
+			res.status(200).send({error:err});
 		}
 		else {
 			next();
