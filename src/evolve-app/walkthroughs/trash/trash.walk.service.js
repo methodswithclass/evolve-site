4169e22983a3e2944f6f53142d2b39a47b64fa8e
Christopher Polito <cpolito343@gmail.com>
2018-08-29 02:10:26 -0400

    update

--- src/evolve-app/walkthroughs/trash/trash.walk.service.js
+++ src/evolve-app/walkthroughs/trash/trash.walk.service.js
@@ -230,7 +230,7 @@ app.factory("trash.walkthrough", ["utility", "phases.service", "control.service"
 	var indicateRefreshButton = function () {
 
 		moveElement({element:"#complete-buttontoggle", top:"#main-inner", buffer:(g.isMobile() ? 2000 : 1700)});
-		moveElement({element:"#phase3-containertoggle", top:"#main-inner", buffer:(g.isMobile() ? 1700 : 1400)});
+		moveElement({element:"#phase3-containertoggle", top:"#main-inner", buffer:(g.isMobile() ? 1900 : 1400)});
 	}
 
 	var phase3 = function () {
