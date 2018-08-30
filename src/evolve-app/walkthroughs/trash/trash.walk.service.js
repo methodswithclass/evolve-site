bcac226198c43f6a306fd4bed486a26b21f6eb39
Christopher Polito <cpolito343@gmail.com>
2018-08-30 13:07:35 -0400

    update

--- src/evolve-app/walkthroughs/trash/trash.walk.service.js
+++ src/evolve-app/walkthroughs/trash/trash.walk.service.js
@@ -190,7 +190,7 @@ app.factory("trash.walkthrough", ["utility", "phases.service", "control.service"
 				setTimeout(function () {
 					u.toggle("hide", "complete-button");
 					toggleGrayout(false);
-				}, 300);
+				}, 600);
 			}
 		}
 	},
