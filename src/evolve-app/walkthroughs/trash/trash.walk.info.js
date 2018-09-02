43dab2328753d68db71a27861a5734eb86859ab7
Christopher Polito <cpolito343@gmail.com>
2018-09-02 12:24:42 -0400

    update

--- src/evolve-app/walkthroughs/trash/trash.walk.info.js
+++ src/evolve-app/walkthroughs/trash/trash.walk.info.js
@@ -7,7 +7,11 @@ app.directive("trashInfo", function () {
 
 			<div class="relative width80 hcenter padding-v-100">
 				<ol>
-					<li>{{action}} the animating New button to the left at the top of the controls to generate a new trash configuration <br>(a good plan will work just as well on any trash configuration, it's relative to the robot, not the grid)</li>
+					<li>{{action}} the animating New button to the left at the top of the controls to generate a new trash configuration
+						<ul>
+							<li>a good plan will work just as well on any trash configuration, it's relative to the robot, not the grid</li>
+						</ul>
+					</li>
 					<li>Rerun the simulation</li>
 				</ol>
 
@@ -16,8 +20,12 @@ app.directive("trashInfo", function () {
 				<ul>
 					<li>Repeat these steps a few times to get a good sense for how well your plan fares</li>
 
-					<li>Then evolve more generations (usually about a 1000 or more) until you reach the maximum fitness (points) for the grid size to see the most intelligent result</li>
-					<li>The max points for the grid size is: (trash percentage) x (number of grid squares) x (10 points for a successful trash pickup)</li>
+					<li>Then evolve more generations (usually about a 1000 or more) until you reach the maximum fitness (score) for the grid size to see the most intelligent result</li>
+				</ul>
+
+				<ul>
+					<li>The max fitness for the grid size is: (trash percentage) x (number of grid squares) x (10 points for a successful trash pickup)</li>
+					<li>So for a 5x5 grid with 50% trash, the max fitness possible is (50%)x(25 squares)x10=120 (rounded down) 
 				</ul>
 			</div>
 
